3368104711173d36c389fe203f89dfa2982585cc
Vincenzo Olivito <vincenzo.olivito@gmail.com>
2017-04-27 17:50:08 +0200

    fix

---
+++ ConditionController/ConditionController/FAQViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FAQViewController.swift
+//  ConditionController
+//
+//  Created by Vincenzo on 27/04/2017.
+//  Copyright © 2017 vincenzoOlivito. All rights reserved.
+//
+
+import UIKit
+
+class FAQViewController: UIViewController {
+    
+    @IBOutlet weak var webView: UIWebView!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let pdf = Bundle.main.url(forResource: "FAQ", withExtension: "pdf")
+        {
+            webView.loadRequest(URLRequest(url: pdf))
+        }
+    }
+}
